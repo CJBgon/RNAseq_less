db89add52c95b9fad203d8e78312be5cc69e412b
Christian Bouwens <48950814+CJBgon@users.noreply.github.com>
2020-05-05 14:46:47 +0100

    Create installpackages.R

---
+++ installpackages.R
@@ -0,0 +1,7 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("edgeR")
+BiocManager::install("tweeDEseqCountData")
+BiocManager::install("TCGAutils")
+BiocManager::install("data.table")
